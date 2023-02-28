72bb2f99175b69937dabddef7dd70a7caa575ffe
JohnLeo-XJTU <57985340+JohnLeo-XJTU@users.noreply.github.com>
2023-02-28 10:46:17 +0800

    Add files via upload

--- nets/darknet.py
+++ nets/darknet.py
@@ -4,6 +4,82 @@
 
 import torch
 from torch import nn
+import math
+
+class ECA(nn.Module):
+    def __init__(self, channel, b=1, gamma=2):
+        super(ECA, self).__init__()
+        kernel_size = int(abs((math.log(channel, 2) + b) / gamma))
+        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
+
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
+'''ATT'''
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.f1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu = nn.ReLU()
+        self.f2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = self.f2(self.relu(self.f1(self.avg_pool(x))))
+        max_out = self.f2(self.relu(self.f1(self.max_pool(x))))
+        out = self.sigmoid(avg_out + max_out)
+        return out
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        x = self.conv(x)
+        return self.sigmoid(x)
+class CBAM(nn.Module):
+    # CSP Bottleneck with 3 convolutions
+    def __init__(self, c1, ratio=16, kernel_size=7):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(c1, ratio)
+        self.spatial_attention = SpatialAttention(kernel_size)
+    def forward(self, x):
+        out = self.channel_attention(x) * x
+        out = self.spatial_attention(out) * out
+        return out
+
+class SE(nn.Module):
+    def __init__(self, c1, r=16):
+        super(SE, self).__init__()
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.l1 = nn.Linear(c1, c1 // r, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+        self.l2 = nn.Linear(c1 // r, c1, bias=False)
+        self.sig = nn.Sigmoid()
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avgpool(x).view(b, c)
+        y = self.l1(y)
+        y = self.relu(y)
+        y = self.l2(y)
+        y = self.sig(y)
+        y = y.view(b, c, 1, 1)
+        return x * y
+'''注意力模块'''
+
+
 
 class SiLU(nn.Module):
     @staticmethod
@@ -76,27 +152,48 @@ class SPPBottleneck(nn.Module):
 #--------------------------------------------------#
 #   残差结构的构建，小的残差结构
 #--------------------------------------------------#
+# class Bottleneck(nn.Module):
+#     # Standard bottleneck
+#     def __init__(self, in_channels, out_channels, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
+#         super().__init__()
+#         hidden_channels = int(out_channels * expansion)
+#         Conv = DWConv if depthwise else BaseConv
+#         #--------------------------------------------------#
+#         #   利用1x1卷积进行通道数的缩减。缩减率一般是50%
+#         #--------------------------------------------------#
+#         self.conv1 = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
+#         #--------------------------------------------------#
+#         #   利用3x3卷积进行通道数的拓张。并且完成特征提取
+#         #--------------------------------------------------#
+#         self.conv2 = Conv(hidden_channels, out_channels, 3, stride=1, act=act)
+#         self.use_add = shortcut and in_channels == out_channels
+#
+#     def forward(self, x):
+#         y = self.conv2(self.conv1(x))
+#         if self.use_add:
+#             y = y + x
+#         return y
+
+
 class Bottleneck(nn.Module):
     # Standard bottleneck
     def __init__(self, in_channels, out_channels, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
         super().__init__()
         hidden_channels = int(out_channels * expansion)
         Conv = DWConv if depthwise else BaseConv
-        #--------------------------------------------------#
-        #   利用1x1卷积进行通道数的缩减。缩减率一般是50%
-        #--------------------------------------------------#
         self.conv1 = BaseConv(in_channels, hidden_channels, 1, stride=1, act=act)
-        #--------------------------------------------------#
-        #   利用3x3卷积进行通道数的拓张。并且完成特征提取
-        #--------------------------------------------------#
         self.conv2 = Conv(hidden_channels, out_channels, 3, stride=1, act=act)
         self.use_add = shortcut and in_channels == out_channels
-
+        '''注意力机制'''
+        self.se = SE(hidden_channels)
     def forward(self, x):
-        y = self.conv2(self.conv1(x))
+        '''注意力机制'''
+        y = self.conv2(self.se(self.conv1(x)))
+        # y = self.conv2(self.conv1(x))
         if self.use_add:
             y = y + x
         return y
+
 
 class CSPLayer(nn.Module):
     def __init__(self, in_channels, out_channels, n=1, shortcut=True, expansion=0.5, depthwise=False, act="silu",):
@@ -145,62 +242,133 @@ class CSPLayer(nn.Module):
         #-----------------------------------------------#
         return self.conv3(x)
 
+# class CSPDarknet(nn.Module):
+#     def __init__(self, dep_mul, wid_mul, out_features=("dark3", "dark4", "dark5"), depthwise=False, act="silu",):
+#         super().__init__()
+#         assert out_features, "please provide output features of Darknet"
+#         self.out_features = out_features
+#         Conv = DWConv if depthwise else BaseConv
+#
+#         #-----------------------------------------------#
+#         #   输入图片是640, 640, 3
+#         #   初始的基本通道是64
+#         #-----------------------------------------------#
+#         base_channels   = int(wid_mul * 64)  # 64
+#         base_depth      = max(round(dep_mul * 3), 1)  # 3
+#
+#         #-----------------------------------------------#
+#         #   利用focus网络结构进行特征提取
+#         #   640, 640, 3 -> 320, 320, 12 -> 320, 320, 64
+#         #-----------------------------------------------#
+#         self.stem = Focus(3, base_channels, ksize=3, act=act)
+#
+#         #-----------------------------------------------#
+#         #   完成卷积之后，320, 320, 64 -> 160, 160, 128
+#         #   完成CSPlayer之后，160, 160, 128 -> 160, 160, 128
+#         #-----------------------------------------------#
+#         self.dark2 = nn.Sequential(
+#             Conv(base_channels, base_channels * 2, 3, 2, act=act),
+#             CSPLayer(base_channels * 2, base_channels * 2, n=base_depth, depthwise=depthwise, act=act),
+#         )
+#
+#         #-----------------------------------------------#
+#         #   完成卷积之后，160, 160, 128 -> 80, 80, 256
+#         #   完成CSPlayer之后，80, 80, 256 -> 80, 80, 256
+#         #-----------------------------------------------#
+#         self.dark3 = nn.Sequential(
+#             Conv(base_channels * 2, base_channels * 4, 3, 2, act=act),
+#             CSPLayer(base_channels * 4, base_channels * 4, n=base_depth * 3, depthwise=depthwise, act=act),
+#         )
+#
+#         #-----------------------------------------------#
+#         #   完成卷积之后，80, 80, 256 -> 40, 40, 512
+#         #   完成CSPlayer之后，40, 40, 512 -> 40, 40, 512
+#         #-----------------------------------------------#
+#         self.dark4 = nn.Sequential(
+#             Conv(base_channels * 4, base_channels * 8, 3, 2, act=act),
+#             CSPLayer(base_channels * 8, base_channels * 8, n=base_depth * 3, depthwise=depthwise, act=act),
+#         )
+#
+#         #-----------------------------------------------#
+#         #   完成卷积之后，40, 40, 512 -> 20, 20, 1024
+#         #   完成SPP之后，20, 20, 1024 -> 20, 20, 1024
+#         #   完成CSPlayer之后，20, 20, 1024 -> 20, 20, 1024
+#         #-----------------------------------------------#
+#         self.dark5 = nn.Sequential(
+#             Conv(base_channels * 8, base_channels * 16, 3, 2, act=act),
+#             SPPBottleneck(base_channels * 16, base_channels * 16, activation=act),
+#             CSPLayer(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False, depthwise=depthwise, act=act),
+#         )
+#
+#     def forward(self, x):
+#         outputs = {}
+#         x = self.stem(x)
+#         outputs["stem"] = x
+#         x = self.dark2(x)
+#         outputs["dark2"] = x
+#         #-----------------------------------------------#
+#         #   dark3的输出为80, 80, 256，是一个有效特征层
+#         #-----------------------------------------------#
+#         x = self.dark3(x)
+#         outputs["dark3"] = x
+#         #-----------------------------------------------#
+#         #   dark4的输出为40, 40, 512，是一个有效特征层
+#         #-----------------------------------------------#
+#         x = self.dark4(x)
+#         outputs["dark4"] = x
+#         #-----------------------------------------------#
+#         #   dark5的输出为20, 20, 1024，是一个有效特征层
+#         #-----------------------------------------------#
+#         x = self.dark5(x)
+#         outputs["dark5"] = x
+#         return {k: v for k, v in outputs.items() if k in self.out_features}
+
 class CSPDarknet(nn.Module):
-    def __init__(self, dep_mul, wid_mul, out_features=("dark3", "dark4", "dark5"), depthwise=False, act="silu",):
+    def __init__(self, dep_mul, wid_mul, out_features=("dark3", "dark4", "dark5"), depthwise=False, act="silu", ):
         super().__init__()
         assert out_features, "please provide output features of Darknet"
         self.out_features = out_features
         Conv = DWConv if depthwise else BaseConv
 
-        #-----------------------------------------------#
-        #   输入图片是640, 640, 3
-        #   初始的基本通道是64
-        #-----------------------------------------------#
-        base_channels   = int(wid_mul * 64)  # 64
-        base_depth      = max(round(dep_mul * 3), 1)  # 3
-        
-        #-----------------------------------------------#
-        #   利用focus网络结构进行特征提取
-        #   640, 640, 3 -> 320, 320, 12 -> 320, 320, 64
-        #-----------------------------------------------#
+        base_channels = int(wid_mul * 64)  # 64
+        base_depth = max(round(dep_mul * 3), 1)  # 3
+
+        # -----------------------------------------------#
+        #   引入cbam注意力机制
+        # -----------------------------------------------#
+        # self.cbam1 = CBAM(base_channels * 4)
+        # self.cbam2 = CBAM(base_channels * 8)
+        # self.cbam3 = CBAM(base_channels * 16)
+        # self.cbam1 = SE(base_channels * 4)
+        # self.cbam2 = SE(base_channels * 8)
+        # self.cbam3 = SE(base_channels * 16)
+        self.cbam1 = ECA(base_channels * 4)
+        self.cbam2 = ECA(base_channels * 8)
+        self.cbam3 = ECA(base_channels * 16)
+
+
         self.stem = Focus(3, base_channels, ksize=3, act=act)
 
-        #-----------------------------------------------#
-        #   完成卷积之后，320, 320, 64 -> 160, 160, 128
-        #   完成CSPlayer之后，160, 160, 128 -> 160, 160, 128
-        #-----------------------------------------------#
         self.dark2 = nn.Sequential(
             Conv(base_channels, base_channels * 2, 3, 2, act=act),
             CSPLayer(base_channels * 2, base_channels * 2, n=base_depth, depthwise=depthwise, act=act),
         )
 
-        #-----------------------------------------------#
-        #   完成卷积之后，160, 160, 128 -> 80, 80, 256
-        #   完成CSPlayer之后，80, 80, 256 -> 80, 80, 256
-        #-----------------------------------------------#
         self.dark3 = nn.Sequential(
             Conv(base_channels * 2, base_channels * 4, 3, 2, act=act),
             CSPLayer(base_channels * 4, base_channels * 4, n=base_depth * 3, depthwise=depthwise, act=act),
         )
 
-        #-----------------------------------------------#
-        #   完成卷积之后，80, 80, 256 -> 40, 40, 512
-        #   完成CSPlayer之后，40, 40, 512 -> 40, 40, 512
-        #-----------------------------------------------#
         self.dark4 = nn.Sequential(
             Conv(base_channels * 4, base_channels * 8, 3, 2, act=act),
             CSPLayer(base_channels * 8, base_channels * 8, n=base_depth * 3, depthwise=depthwise, act=act),
         )
 
-        #-----------------------------------------------#
-        #   完成卷积之后，40, 40, 512 -> 20, 20, 1024
-        #   完成SPP之后，20, 20, 1024 -> 20, 20, 1024
-        #   完成CSPlayer之后，20, 20, 1024 -> 20, 20, 1024
-        #-----------------------------------------------#
         self.dark5 = nn.Sequential(
             Conv(base_channels * 8, base_channels * 16, 3, 2, act=act),
             SPPBottleneck(base_channels * 16, base_channels * 16, activation=act),
-            CSPLayer(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False, depthwise=depthwise, act=act),
+            CSPLayer(base_channels * 16, base_channels * 16, n=base_depth, shortcut=False, depthwise=depthwise,
+                     act=act),
         )
 
     def forward(self, x):
@@ -209,21 +377,24 @@ class CSPDarknet(nn.Module):
         outputs["stem"] = x
         x = self.dark2(x)
         outputs["dark2"] = x
-        #-----------------------------------------------#
-        #   dark3的输出为80, 80, 256，是一个有效特征层
-        #-----------------------------------------------#
+        # ---------------------------------------------------------#
+        #   dark3的输出为80, 80, 256，是一个有效特征层，引入cbam模块
+        # ---------------------------------------------------------#
         x = self.dark3(x)
-        outputs["dark3"] = x
-        #-----------------------------------------------#
-        #   dark4的输出为40, 40, 512，是一个有效特征层
-        #-----------------------------------------------#
+        x1 = self.cbam1(x)
+        outputs["dark3"] = x1
+        # --------------------------------------------------------#
+        #   dark4的输出为40, 40, 512，是一个有效特征层，引入cbam模块
+        # --------------------------------------------------------#
         x = self.dark4(x)
-        outputs["dark4"] = x
-        #-----------------------------------------------#
-        #   dark5的输出为20, 20, 1024，是一个有效特征层
-        #-----------------------------------------------#
+        x2 = self.cbam2(x)
+        outputs["dark4"] = x2
+        # --------------------------------------------------------#
+        #   dark5的输出为20, 20, 1024，是一个有效特征层，引入cbam模块
+        # --------------------------------------------------------#
         x = self.dark5(x)
-        outputs["dark5"] = x
+        x3 = self.cbam3(x)
+        outputs["dark5"] = x3
         return {k: v for k, v in outputs.items() if k in self.out_features}
 
 
